3e43aca525fd529cfdc289c350352a97990c320a
Ivan Yakimov <legendthechaos@gmail.com>
2025-10-05 00:32:00 +0500

    AddedNpc spawning, fixed root motion sliding

---
+++ src/LDJam58/Assets/Scripts/Game/NPC/SpawnNpcs.cs
@@ -0,0 +1,12 @@
+﻿namespace Game.NPC
+{
+    public class SpawnNpcs
+    {
+        public int NpcCount;
+
+        public SpawnNpcs(int npcCount)
+        {
+            NpcCount = npcCount;
+        }
+    }
+}
