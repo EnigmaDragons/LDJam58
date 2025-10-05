e1b1429f2333d30b7cbf6ce449813a4970e73317
Silas Reinagel <silas.reinagel@gmail.com>
2025-10-05 16:47:28 -0500

    Picker UI: Rarity Frames. Closes #147

--- src/LDJam58/Assets/Scripts/UI/ExhibitPickerView.cs
+++ src/LDJam58/Assets/Scripts/UI/ExhibitPickerView.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using Assets.Scripts;
-using System.Linq;
 using Game.Messages;
 
 public class ExhibitPickerView : MonoBehaviour
 {
     [SerializeField] private Image _exhibitImage;
     [SerializeField] private TextMeshProUGUI _exhibitNameLabel;
+    [SerializeField] private Image _frameImage;
+    [SerializeField] private Image _backFrameImage;
 
     [SerializeField] private TextMeshProUGUI _sizeLabel;
     [SerializeField] private TextMeshProUGUI _rarityLabel;
@@ -16,6 +16,16 @@ public class ExhibitPickerView : MonoBehaviour
     [SerializeField] private OneToElevenMeter _popularityMeter;
     [SerializeField] private TagsDisplayView _tagsView;
     [SerializeField] private Button _pickButton;
+    
+    [SerializeField] private Sprite _commonFrameSprite;
+    [SerializeField] private Sprite _rareFrameSprite;
+    [SerializeField] private Sprite _exoticFrameSprite;
+    [SerializeField] private Sprite _mythicFrameSprite;
+    
+    [SerializeField] private Sprite _commonBackFrameSprite;
+    [SerializeField] private Sprite _rareBackFrameSprite;
+    [SerializeField] private Sprite _exoticBackFrameSprite;
+    [SerializeField] private Sprite _mythicBackFrameSprite;
     
     private ExhibitTileType _exhibitTileType;
     public void Init(ExhibitTileType exhibits)
@@ -32,6 +42,8 @@ public class ExhibitPickerView : MonoBehaviour
         _rarityLabel.text = exhibits.Rarity.ToString();
         _popularityMeter.SetValue(exhibits.Popularity);
         _tagsView.SetTags(exhibits.Tags);
+        
+        UpdateRarityFrame();
     }
     
     private Sprite GetDefaultSprite()
@@ -39,6 +51,33 @@ public class ExhibitPickerView : MonoBehaviour
         // Try to load a default sprite from Resources or use Unity's default
         var defaultSprite = Resources.Load<Sprite>("DefaultExhibitSprite");
         return defaultSprite;
+    }
+
+    private void UpdateRarityFrame()
+    {
+        if (_frameImage != null)
+        {
+            _frameImage.sprite = _exhibitTileType.Rarity switch
+            {
+                ExhibitRarity.Common => _commonFrameSprite,
+                ExhibitRarity.Rare => _rareFrameSprite,
+                ExhibitRarity.Exotic => _exoticFrameSprite,
+                ExhibitRarity.Mythic => _mythicFrameSprite,
+                _ => _commonFrameSprite
+            };
+        }
+        
+        if (_backFrameImage != null)
+        {
+            _backFrameImage.sprite = _exhibitTileType.Rarity switch
+            {
+                ExhibitRarity.Common => _commonBackFrameSprite,
+                ExhibitRarity.Rare => _rareBackFrameSprite,
+                ExhibitRarity.Exotic => _exoticBackFrameSprite,
+                ExhibitRarity.Mythic => _mythicBackFrameSprite,
+                _ => _commonBackFrameSprite
+            };
+        }
     }
 
     private void PickExhibit()
