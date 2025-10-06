868a7b4ee6c4a594ada353a1f580cc6a5cb00f68
Noah Reinagel <noah.r9@gmail.com>
2025-10-06 03:40:44 -0700

    rooms now have ceilings and automatically hide or show themselves based on season

---
+++ src/LDJam58/Assets/Scripts/Game/Rooms/RoomOpensOnSeason.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class RoomOpensOnSeason : OnMessage<SeasonInitialized>
+{
+    [SerializeField] private RoomId roomId;
+    [SerializeField] private GameObject ceiling;
+    [SerializeField] private GameObject placementGrid;
+    [SerializeField] private int seasonToOpen = 1;
+
+    private bool presentlyOpen = true;
+    
+    protected override void Execute(SeasonInitialized msg)
+    {
+        CurrentGameState.UpdateState(state =>
+        {
+            if (!state.Rooms.ContainsKey(roomId.Id))
+                state.Rooms[roomId.Id] = new RoomState();
+            var open = state.currentSeasonIndex >= seasonToOpen - 1;
+            if (open == presentlyOpen)
+                return;
+            presentlyOpen = open;
+            state.Rooms[roomId.Id].open = open;
+            ceiling.SetActive(!presentlyOpen);
+            placementGrid.SetActive(presentlyOpen);
+        });
+    }
+}
