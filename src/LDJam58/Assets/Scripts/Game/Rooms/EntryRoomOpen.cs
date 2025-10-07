daaab673c2ad5c77cc3745a31a98d353b97efbda
Silas Reinagel <silas.reinagel@gmail.com>
2025-10-07 09:29:44 -0500

    Bug Fix: Room Open State

--- src/LDJam58/Assets/Scripts/Game/Rooms/EntryRoomOpen.cs
+++ src/LDJam58/Assets/Scripts/Game/Rooms/EntryRoomOpen.cs
@@ -4,5 +4,14 @@ public class EntryRoomOpen : MonoBehaviour
 {
     [SerializeField] private BoxCollider bounds;
 
-    void Start() => Message.Publish(new RoomOpened {RoomId = "entry", Bounds = bounds.bounds});
+    void Start()
+    {
+        CurrentGameState.UpdateState(state =>
+        {
+            if (!state.Rooms.ContainsKey("entry"))
+                state.Rooms["entry"] = new RoomState();
+            state.Rooms["entry"].open = true;
+        });
+        Message.Publish(new RoomOpened {RoomId = "entry", Bounds = bounds.bounds});
+    }
 }
